a6ea53056b961a9aaf71af3bd391279381a5c7ac
Cameron Weston <cameronbweston@gmail.com>
2021-07-23 17:23:02 -0500

    Create an object of 10 random songs playlist that is pulled and gathered from the spotify API. Pass this array of objects back to the showPlaylist view.

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -6,49 +6,64 @@ export {
     create
 }
 
-function create(req, res) {
+const playlistNames = ['In my feels', 'Rad Mix', 'Bonkers', 'Soul Soother']
 
+function create(req, res) {
+    let reqHeaders = {
+        headers: {
+        Authorization: 'Bearer ' + process.env.ACCESS_TOKEN //the token is a variable which holds the token
+        }
+    }
     axios.all([
-        axios.get('https://api.spotify.com/v1/search?q=John%20Mayer&type=artist', {
-                    headers: {
-                    Authorization: 'Bearer ' + process.env.ACCESS_TOKEN //the token is a variable which holds the token
-                    }
-        }),
-        axios.get('https://api.spotify.com/v1/search?q=Daft%20Punk&type=artist', {
-                headers: {
-                Authorization: 'Bearer ' + process.env.ACCESS_TOKEN //the token is a variable which holds the token
-                }
-        }),
-        axios.get('https://api.spotify.com/v1/search?q=Jim%20Croce&type=artist', {
-            headers: {
-            Authorization: 'Bearer ' + process.env.ACCESS_TOKEN //the token is a variable which holds the token
-            }
-        })
+        axios.get('https://api.spotify.com/v1/search?q=John%20Mayer&type=artist', reqHeaders),
+        axios.get('https://api.spotify.com/v1/search?q=Daft%20Punk&type=artist', reqHeaders),
+        axios.get('https://api.spotify.com/v1/search?q=Jim%20Croce&type=artist', reqHeaders)
     ])
     .then(axios.spread((...responses) => {
-        console.log(responses[0].data.artists.items[0].id)
-        console.log(responses[1].data.artists.items[0].id)
-        console.log(responses[2].data.artists.items[0].id)
+        // console.log(responses[0].data.artists.items[0].id)
+        // console.log(responses[1].data.artists.items[0].id)
+        // console.log(responses[2].data.artists.items[0].id)
         const artistIds =[]
 
-        artistIds.push(responses[0].data.artists.items[0].id)
-        artistIds.push(responses[1].data.artists.items[0].id)
-        artistIds.push(responses[2].data.artists.items[0].id)
+        artistIds.push(responses[0].data.artists.items[0].id, responses[1].data.artists.items[0].id, responses[2].data.artists.items[0].id)
         
         return artistIds
     }))
     .then((artistIds) => {
-        let id = artistIds[0]
-        axios.get(`https://api.spotify.com/v1/artists/${id}`, {
-            headers: {
-            Authorization: 'Bearer ' + process.env.ACCESS_TOKEN //the token is a variable which holds the token
-            }
+        const recommendedRandomPlaylist = [{
+            songId,
+            songName,
+            artistName,
+            imageUrl,
+        }]
+
+        axios.get(`https://api.spotify.com/v1/recommendations?limit=10&market=ES&seed_artists=${artistIds[0]}%2C${artistIds[1]}%2C${artistIds[2]}`, reqHeaders)
+        .then(listOfTracks => {
+            console.log(listOfTracks.data.tracks)
+            listOfTracks.data.tracks.forEach(track => {
+                console.log(track.id)
+                console.log(`image url: ${track.album.images[0].url}`)
+                console.log(track.album.artists.name)
+                console.log(track.name)
+                
+                let song = {
+                    songId: track.id,
+                    songName: track.name,
+                    artistName: track.album.artists.name,
+                    imageUrl: track.album.images[0].url
+                }
+                recommendedRandomPlaylist.push(song)
+
+                res.render('/playlists/showPlaylist', {
+                    recommendedRandomPlaylist, 
+                })
+            })
         })
-        .then(req => {
-            console.log(req.data)
-        })
-        .catch(err => {
-            console.log(err)
+    })
+    .catch(err => {
+        console.log(err)
+        res.redirect('/', {
+            error
         })
     })
 
