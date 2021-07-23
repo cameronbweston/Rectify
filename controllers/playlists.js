78c8e9f5116fce7111bfb373dacfae0485742cb4
Cameron Weston <cameronbweston@gmail.com>
2021-07-23 17:42:38 -0500

    Make sure that the view is being rendered correctly and passing back our playlist objects

--- controllers/playlists.js
+++ controllers/playlists.js
@@ -30,41 +30,42 @@ function create(req, res) {
         return artistIds
     }))
     .then((artistIds) => {
-        const recommendedRandomPlaylist = [{
-            songId,
-            songName,
-            artistName,
-            imageUrl,
-        }]
 
         axios.get(`https://api.spotify.com/v1/recommendations?limit=10&market=ES&seed_artists=${artistIds[0]}%2C${artistIds[1]}%2C${artistIds[2]}`, reqHeaders)
         .then(listOfTracks => {
             console.log(listOfTracks.data.tracks)
+
+            //Store our 10 song objects
+            const recommendedRandomPlaylist = []
+
             listOfTracks.data.tracks.forEach(track => {
                 console.log(track.id)
                 console.log(`image url: ${track.album.images[0].url}`)
-                console.log(track.album.artists.name)
+                console.log(track.album.artists[0].name)
                 console.log(track.name)
                 
                 let song = {
                     songId: track.id,
                     songName: track.name,
-                    artistName: track.album.artists.name,
+                    artistName: track.album.artists[0].name,
                     imageUrl: track.album.images[0].url
                 }
                 recommendedRandomPlaylist.push(song)
 
-                res.render('/playlists/showPlaylist', {
-                    recommendedRandomPlaylist, 
-                })
+                // res.render('playlists/showPlaylist', {
+                //     recommendedRandomPlaylist, 
+                // })
+            })
+        })
+        .then(recommendedRandomPlaylist => {
+            res.render('playlists/showPlaylist', {
+                recommendedRandomPlaylist, 
             })
         })
     })
     .catch(err => {
         console.log(err)
-        res.redirect('/', {
-            error
-        })
+        //res.redirect('/')
     })
 
 }
